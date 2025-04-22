3fa68459c755e41555ec6ff199e624c0a2fb853d
ricomateo <mrico@fi.uba.ar>
2025-04-21 23:25:32 -0300

    agrego campo timestamp a QueryPacket

--- deliver/src/deliver.py
+++ deliver/src/deliver.py
@@ -130,6 +130,7 @@ class DeliverNode:
                 if handle_final_packet(method, self.input_rabbitmq):
                     response_str = self._generate_response()
                     query_packet = QueryPacket(
+                        timestamp=datetime.utcnow().isoformat(),
                         data={"source": self.input_queue},
                         response=response_str
                     )
